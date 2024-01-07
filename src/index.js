f50a504b217d8ef417cf45643684911d00a5b2a6
Levis <Ngigilevis@gmail.com>
2024-01-07 20:33:27 +0300

    done with assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.addEventListener('DOMContentLoaded', () => {
+  let form = document.querySelector('#create-task-form');
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+  let newTodo = event.target['new-task-description'].value;
+    console.log(newTodo);
+    Todo(newTodo);
+    form.reset();
+  });
 });
+
+function Todo(todo) {
+  const li = document.createElement('li');
+  const btn = document.createElement('button');
+  btn.addEventListener('click', handleDelete);
+  li.addEventListener('mouseover', () => setColor(li, 'Urgent')); // Example priority value
+  btn.innerText = 'x';
+  li.innerHTML = `${todo} `; // Example priority value
+  li.appendChild(btn);
+
+  document.querySelector('#tasks').appendChild(li);
+}
+
+function setColor(li, priority) {
+  if (priority === 'Urgent') {
+    li.style.color = "#cccfffggg";
+  } else if (priority === 'Less Urgent') {
+    li.style.color = "green";
+  }
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove();
+}
